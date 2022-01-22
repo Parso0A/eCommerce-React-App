e58133eaddff45526457ab2fdb31c1e0dd825db6
Parso0A <parsarashidikia@gmail.com>
2022-01-22 20:43:09 +0330

    remainders

--- src/components/user/UserDashboard.jsx
+++ src/components/user/UserDashboard.jsx
@@ -1,6 +1,5 @@
 import React from "react";
 import Layout from "../core/Layout";
-import { isAuthenticated } from "../../services/auth/authService";
 import { Link } from "react-router-dom";
 import { selectUser } from "../../store/auth";
 import { useSelector } from "react-redux";
